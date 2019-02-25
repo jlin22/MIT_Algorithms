a797a5fae6b8ff0c630b4fa98ca596dd0fc01b68
John Lin <jlin1674@gmail.com>
2019-02-24 21:25:19 -0500

    finished binary insertion sort

--- Unit2/basic_sorts.py
+++ Unit2/basic_sorts.py
@@ -1,5 +1,10 @@
 import time
 
+def swap(a, p, q):
+    temp = a[p]
+    a[p] = a[q]
+    a[q] = temp
+
 def time_func(func):
     def modified_func(inputs):
         start = time.time()
@@ -9,7 +14,6 @@ def time_func(func):
         return output
     return modified_func
 
-@time_func
 def insertion_sort(a):
     for i in range(1, len(a)):
         key = a[i]
@@ -22,6 +26,31 @@ def insertion_sort(a):
 
 a = [31, 64, 45, 28, 12, 50]
 print(insertion_sort(a))
+
+def binary_search(a, key, low, high):
+    while low <= high:
+        mid = (low + high) // 2
+        if key < a[mid]:
+            high = mid-1
+        elif key > a[mid]:
+            low = mid+1
+        else:
+            return mid
+    return high
+
+print(binary_search(a, 44, 0, len(a)))
+
+def binary_insertion_sort(a):
+    for i in range(1, len(a)):
+        j = binary_search(a, a[i], 0, i-1)
+        k = i-1
+        while k > j:
+            swap(a, k+1, k)
+            k -= 1
+    return a
+
+a = [31, 64, 45, 28, 12, 50]
+print(binary_insertion_sort(a))
 
 def merge_sort(a):
     return merge_sort_helper(a, 0, len(a)-1)
