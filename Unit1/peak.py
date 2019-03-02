dfa0875854728e0f67c71e28c156969caadddb3d
John Lin <jlin1674@gmail.com>
2019-03-02 10:50:05 -0500

    fast_peakfinder

--- Unit1/peak.py
+++ Unit1/peak.py
@@ -13,4 +13,24 @@ def linear_peakfinder(a):
     # Never reaches this point
     return -1
 
+def fast_peakfinder(a):
+    return fast_pf_helper(a, 0, len(a) - 1)
 
+def fast_pf_helper(a, start, end):
+    if start > end:
+        return -1
+    mid = (start + end) // 2
+    if in_boundary(mid-1, start, end) and a[mid - 1] >= a[mid]:
+        return fast_pf_helper(a, start, mid-1)
+    elif in_boundary(mid+1, start, end) and a[mid + 1] >= a[mid]:
+        return fast_pf_helper(a, mid+1, end)
+    else:
+        return mid
+    
+def in_boundary(ind, start, end):
+    return ind >= start and ind <= end
+
+if __name__ == "__main__":
+    a = [1, 2, 3, 4]
+    print(fast_peakfinder(a))
+    
