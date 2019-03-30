216f852d4882d322a67b2b355a35b34ada392cb2
John Lin <jlin1674@gmail.com>
2019-03-30 17:53:52 -0400

    height for avl tree

---
+++ Unit2/avl_sort.py
@@ -0,0 +1,8 @@
+def height(node):
+    if node is None:
+        return -1
+    else:
+        return node.height
+
+def update_height(node):
+    node.height = max(height(node.left), height(node.right)) + 1
