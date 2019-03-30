17d54ee7e2d4b81aabbc84ec6ff980fe14b25a0e
John Lin <jlin1674@gmail.com>
2019-03-30 13:43:17 -0400

    finished selection sort

--- Unit2/other_sorts.py
+++ Unit2/other_sorts.py
@@ -1,3 +1,5 @@
+import random
+
 def swap(a, i, j):
     a[i], a[j] = a[j], a[i]
     return a
@@ -13,6 +15,21 @@ def bubble_sort(a):
             break
     return a
 
+def selection_sort(a):
+    for i in range(0, len(a)):
+        min_index = i
+        for j in range(i+1, len(a)):
+            if a[min_index] > a[j]:
+                min_index = j
+        swap(a, min_index, i)
+    return a
+
+def shuffle(a):
+    for i in range(1, len(a)):
+        r = int(random.randint(0, i))
+        swap(a, i, r)
+    return a
+        
 if __name__ == "__main__":
     a = [1, 5, 6, 7]
     a = swap(a, 1, 2)
@@ -20,4 +37,6 @@ if __name__ == "__main__":
     print(bubble_sort(a))
     b = [100, 65, 45, 33, 22, 11, 9, 8, 7]
     print(bubble_sort(b))
+    print("shuffled", shuffle(b))
+    print(selection_sort(b))
     
