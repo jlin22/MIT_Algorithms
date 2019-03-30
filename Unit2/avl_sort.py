347bbd3db29a6f1858a68f52b0ae492773dca1b2
John Lin <jlin1674@gmail.com>
2019-03-30 18:06:51 -0400

    rebalance function

--- Unit2/avl_sort.py
+++ Unit2/avl_sort.py
@@ -6,3 +6,22 @@ def height(node):
 
 def update_height(node):
     node.height = max(height(node.left), height(node.right)) + 1
+
+def rebalance(node):
+    while node is not None:
+        update_height(node)
+        if left_heavy(node):
+            if left_heavy(node.left) or balanced(node.left):
+                right_rotate(node)
+            else:
+                left_rotate(node.left)
+                right_rotate(node)
+        elif right_heavy(node):
+            if right_heavy(node.right) or balanced(node.right):
+                left_rotate(node)
+            else:
+                right_rotate(node.right)
+                left_rotate(node)
+        node = node.parent
+
+
