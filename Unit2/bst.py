307a36b2f73fd190923298cc2111d1e7dd3b3c21
John Lin <jlin1674@gmail.com>
2019-04-01 21:58:17 -0400

    finished most elementary bst

--- Unit2/bst.py
+++ Unit2/bst.py
@@ -41,10 +41,47 @@ def inorder(root):
     print(root.key)
     inorder(root.right)
 
+def find_min(root):
+    current = root
+    while current.left is not None:
+        current = current.left
+    return current
+
+def next_larger(root):
+    if root.right is not None:
+        return find_min(root.right)
+    else:
+        y, x = root.parent, root
+        while y is not None and y.right == x:
+            x = y
+            y = y.parent
+        return y
+
+def delete(root):
+    if root.right is None or root.left is None:
+        if root is root.parent.left:
+            root.parent.left = root.right or root.left
+            if root.parent.left is not None:
+                root.parent.left.parent = root.parent
+        else:
+            root.parent.right = root.right or root.left
+            if root.parent.right is not None:
+                root.parent.right.parent = root.parent
+        return root
+    else:
+        s = next_larger(root)
+        root.key, s.key = s.key, root.key
+        return delete(root)
+
 if __name__ == "__main__":
     bst = Node()
     a = [1, 2, 3, 4, 5]
     for key in a:
         insert(bst, key)
     inorder(bst)
-    print(find(bst, 4).key)
+    print('key of find(bst,4):', find(bst, 4).key)
+    print('min of bst:',find_min(bst).key)
+    print('next larger of find 3:', next_larger(find(bst, 3)).key)
+    print('delete 4:', delete(find(bst, 4)))
+    print('inorder')
+    inorder(bst)
