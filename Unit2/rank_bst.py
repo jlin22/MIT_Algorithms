70d4c6d83ab331b06c7f273e76feaa0e52aa5b83
John Lin <jlin1674@gmail.com>
2019-04-09 17:43:52 -0400

    commit

--- Unit2/rank_bst.py
+++ Unit2/rank_bst.py
@@ -12,23 +12,20 @@ def insert(root, key):
     new_node.key = key
     current = root
     while True:
+        current.size += 1
         if current.key < key:
             if current.right is None:
-                current.size += 1
                 current.right = new_node
                 new_node.parent = current
                 return new_node
             else:
-                current.size += 1
                 current = current.right
         elif current.key > key:
             if current.left is None:
-                current.size += 1
                 current.left = new_node
                 new_node.parent = current
                 return new_node
             else:
-                current.size += 1
                 current = current.left
         else:
             return None
@@ -42,6 +39,25 @@ def inorder(root, sizes=False):
     else:
         print(root.key)
     inorder(root.right, sizes)
+
+def delete(root, node):
+    if root.left is None or root.right is None:
+        if root == root.parent.left:
+            root.parent.left = root.left or root.right
+            if root.parent.left is not None:
+                root.parent.left.parent = root.parent
+        else:
+            root.parent.right = root.left or root.right
+            if root.parent.right is not None:
+                root.parent.right = root.parent
+        current = root.parent
+        while current is not None:
+            current.size -= 1
+            current = current.parent
+    else:
+        s = next_larger(root)
+        root.key, s.key = s.key, root.key
+        return delete(root)
 
 if __name__ == "__main__":
     bst = RankNode()
